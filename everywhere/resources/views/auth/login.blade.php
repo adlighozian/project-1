0415f738848392a396227e64360dfe1587039b82
adlighozian <adlighozian1@gmail.com>
2021-09-25 12:41:17 +0700

    1 | Login

---
+++ everywhere/resources/views/auth/login.blade.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+
+    <!-- Style CSS -->
+    <link rel="stylesheet" href="/css/login.css">
+
+    <title>KepalaSuku | Login</title>
+</head>
+
+<body>
+
+    <form class="box" action="" action="" method="post">
+        <h1>Login</h1>
+        <input type="text" name="" placeholder="username">
+        <input type="password" name="" placeholder="password">
+        <input type="submit" name="" value="login">
+    </form>
+
+</body>
+
+</html>
